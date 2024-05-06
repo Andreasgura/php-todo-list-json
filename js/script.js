3279067711a6948647095c097d695dbf003f693b
Andreasgura <andre-56@hotmail.it>
2024-05-06 17:25:55 +0200

    added data in json

--- js/script.js
+++ js/script.js
@@ -1,52 +1,66 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 createApp({
-    data() {
-      return {
-        toDo: [],
-        apiUrl: 'server.php',
-        text : ''
+  data() {
+    return {
+      toDo: [],
+      apiUrl: 'server.php',
+      text: ''
+    }
+  },
+  methods: {
+    toggleDone(id) {
+      const item = this.toDo.find((el) => el.id === id);
+      if (item) {
+        item.done = !item.done;
       }
     },
-    methods : {
-      toggleDone(id){
-        const item = this.toDo.find((el) => el.id === id);
-        if (item){
-          item.done = !item.done;
+    removeItem(id) {
+      const i = this.toDo.findIndex((el) => el.id === id)
+      this.toDo.splice(i, 1)
+    },
+    addNewLi(text) {
+      let newId = 0
+      this.toDo.forEach(el => {
+        if (el.id > newId) {
+          newId = el.id
         }
-      },
-      removeItem(id){
-        const i = this.toDo.findIndex((el) => el.id === id)
-        this.toDo.splice(i,1)
-      },
-      addNewLi(text){
-        let newId = 0
-        this.toDo.forEach(el => {
-          if (el.id > newId) {
-            newId = el.id
-          }
-          newId = newId + 1;
-        });
-        let newLi = {
-          id: newId,
-          text,
-          done : false        
+        newId += 1;
+      });
+      const newLi = {
+        id: newId,
+        text,
+        done: false
       };
+
       // console.log(newId);
-      this.toDo.push(newLi);
-      this.text = ''       
-      },
-      getData (){
-        axios
-        .get(this.apiUrl)
+
+      // this.toDo.push(newLi);
+      this.text = '';
+      const data = new FormData();
+      for (let key in newLi) {
+        data.append(key, newLi[key]);
+        // console.log(key, newLi[key]);
+      }
+      console.log(data);
+      axios
+        .post(this.apiUrl, data)
         .then((response) => {
           console.log(response.data);
           this.toDo = response.data
         })
-      }
     },
-    created () {
-     this.getData()
-    },
-    mounted () {      
+    getData() {
+      axios
+        .get(this.apiUrl)
+        .then((response) => {
+          // console.log(response.data);
+          this.toDo = response.data
+        })
     }
-  }).mount('#app');
+  },
+  created() {
+    this.getData();
+  },
+  mounted() {
+  }
+}).mount('#app');
--- server.php
+++ server.php
@@ -2,5 +2,19 @@
 
 $dataJson = file_get_contents("js/data.json");
 
+$listPhp = json_decode($dataJson, true);
+
+if (isset($_POST['text'])) {
+    $newLi = [
+        "id" => $listPhp[count($listPhp) - 1]['id'] + 1,
+        "text" => $_POST['text'],
+        "done" => false
+    ];
+    $listPhp[] = $newLi;
+    $dataJson = json_encode($listPhp, JSON_PRETTY_PRINT);
+    file_put_contents("js/data.json", $dataJson);
+}
+
+
 header("Content-Type: application/json");
 echo $dataJson;
